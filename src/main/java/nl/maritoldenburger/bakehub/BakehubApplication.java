0d82f4602ff46ffc2146cb69afed68c64dd277ce
moldenburger <maritoldenburger@hotmail.com>
2025-11-12 15:26:58 +0100

    Created folder structure, adjusted application.properties and pom.xml

---
+++ src/main/java/nl/maritoldenburger/bakehub/BakehubApplication.java
@@ -0,0 +1,13 @@
+package nl.maritoldenburger.bakehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/maritoldenburger/bakehub/BakehubApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.maritoldenburger.bakehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
